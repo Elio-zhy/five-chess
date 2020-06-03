b3b01b066d23251554568527086969cfb2e1e953
zHhhhhy <zhyUniverse@outlook.com>
2020-06-03 23:49:07 +0800

    Update views.py

--- api/v1/chess/views.py
+++ api/v1/chess/views.py
@@ -6,11 +6,11 @@
 @Author     :   Elio Zhou
 """
 
-from flask import request, render_template
+from flask import request, render_template, Response
 from flask_restful import Resource
 
 
 class Index(Resource):
     @staticmethod
     def get():
-        return render_template('index.html')
+        return Response(render_template('index.html'), content_type='text/html')
